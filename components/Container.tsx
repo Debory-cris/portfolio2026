963578f03ff4f35c5b46f147b8f8f07d754bdc01
Dede <dede@Dedes-MBP.Dlink>
2026-05-08 22:22:27 -0300

    fix: add className prop to Container

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from "react";
 
 type Props = {
     children: ReactNode;
+    className?: string;
 };
 
-export default function Container({ children }: Props) {
+export default function Container({ children, className }: Props) {
     return (
-        <div className="max-w-7xl w-full mx-auto px-6 md:px-10 lg:px-10">
+        <div className={`max-w-7xl w-full mx-auto px-6 md:px-10 lg:px-10 ${className ?? ""}`}>
             {children}
         </div>
     );
